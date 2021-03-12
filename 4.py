fab65d190ac584a6b11f1c27fedbf0e0c3e77045
anselmo-2010 <42847646+anselmo-2010@users.noreply.github.com>
2021-03-12 22:35:31 +0600

    make changes

--- 4.py
+++ 4.py
@@ -1,3 +1,5 @@
+years = int(input("Введите год : "))
+
 def is_year_leap(year):
     if (year 
     % 4 == 0) and (year % 100 != 0) or (year % 400 == 0):
@@ -5,9 +7,5 @@ def is_year_leap(year):
     else:
         print(year, "Год не високосный")
 
-is_year_leap(2014)
-is_year_leap(1984)
-is_year_leap(1988)
-is_year_leap(2019)
-is_year_leap(2020)
+is_year_leap(years)
 
